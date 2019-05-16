27a214d5cd2b9adc1883dceeca425efa89293618
Jeremy Gosselin <gosselin.jrmy@gmail.com>
2019-05-17 00:50:37 +0200

    Refactored small and large straight, first attempt at refactoring full house

--- src/Yatzy.java
+++ src/Yatzy.java
@@ -87,29 +87,15 @@ public class Yatzy
     public static int smallStraight(int d1, int d2, int d3, int d4, int d5)
     {
         int[] dices = new int[] {d1, d2, d3, d4, d5};
-        int[] counts = getValueOccurrences(dices);
-
-        if (counts[0] == 1 &&
-                counts[1] == 1 &&
-                counts[2] == 1 &&
-                counts[3] == 1 &&
-                counts[4] == 1)
-            return 15;
-        return 0;
+        int smallStraightSum = 15;
+        return getSumOfAllTuples(dices,1,5) == smallStraightSum ? smallStraightSum : 0;
     }
 
     public static int largeStraight(int d1, int d2, int d3, int d4, int d5)
     {
         int[] dices = new int[] {d1, d2, d3, d4, d5};
-        int[] counts = getValueOccurrences(dices);
-
-        if (counts[1] == 1 &&
-                counts[2] == 1 &&
-                counts[3] == 1 &&
-                counts[4] == 1
-                && counts[5] == 1)
-            return 20;
-        return 0;
+        int largeStraightSum = 20;
+        return getSumOfAllTuples(dices,1,5) == largeStraightSum ? largeStraightSum : 0;
     }
 
     public static int fullHouse(int d1, int d2, int d3, int d4, int d5)
@@ -117,26 +103,28 @@ public class Yatzy
         int[] dices = new int[] {d1, d2, d3, d4, d5};
         int[] counts = getValueOccurrences(dices);
 
-        boolean _2 = false;
-        int i;
-        int _2_at = 0;
-        boolean _3 = false;
-        int _3_at = 0;
+        boolean pairFound = false;
+        int pairValue = 0;
+        boolean tripleFound = false;
+        int tripleValue = 0;
 
-        for (i = 0; i != 6; i += 1)
-            if (counts[i] == 2) {
-                _2 = true;
-                _2_at = i+1;
+        for (int i = 0; i < counts.length; i ++)
+        {
+            if (counts[i] == 2)
+            {
+                pairFound = true;
+                pairValue = i + 1;
             }
-
-        for (i = 0; i != 6; i += 1)
-            if (counts[i] == 3) {
-                _3 = true;
-                _3_at = i+1;
+            if (counts[i] == 3)
+            {
+                tripleFound = true;
+                tripleValue = i+1;
             }
+        }
 
-        if (_2 && _3)
-            return _2_at * 2 + _3_at * 3;
+
+        if (pairFound && tripleFound)
+            return pairValue * 2 + tripleValue * 3;
         else
             return 0;
     }
@@ -188,6 +176,7 @@ public class Yatzy
             }
         }
 
+        // this check is needed in the case we look for two pairs but find only one
         if(tuplesCounted == expectedTupleCount)
         {
             return sum;
