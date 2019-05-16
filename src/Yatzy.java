8aef1d5fd345b9d40c7fd1f5c8cc45e62cb3b301
Jeremy Gosselin <gosselin.jrmy@gmail.com>
2019-05-16 20:05:56 +0200

    Factorized a method that counts dice value occurences and used it in every method that needed it

--- src/Yatzy.java
+++ src/Yatzy.java
@@ -31,11 +31,7 @@ public class Yatzy
 
     public static int yatzy(int... dices)
     {
-        int[] counts = new int[6];
-        for (int dice : dices)
-        {
-            counts[dice-1]++;
-        }
+        int[] counts = getValueOccurrences(dices);
 
         for(int i = 0; i != 6; i++)
         {
@@ -67,27 +63,20 @@ public class Yatzy
 
     public static int score_pair(int d1, int d2, int d3, int d4, int d5)
     {
-        int[] counts = new int[6];
-        counts[d1-1]++;
-        counts[d2-1]++;
-        counts[d3-1]++;
-        counts[d4-1]++;
-        counts[d5-1]++;
-        int at;
-        for (at = 0; at != 6; at++)
-            if (counts[6-at-1] >= 2)
-                return (6-at)*2;
+        int[] dices = new int[] {d1, d2, d3, d4, d5};
+        int[] counts = getValueOccurrences(dices);
+
+        for (int i = 0; i < 6; i++)
+            if (counts[5-i] >= 2)
+                return (6-i)*2;
         return 0;
     }
 
     public static int two_pair(int d1, int d2, int d3, int d4, int d5)
     {
-        int[] counts = new int[6];
-        counts[d1-1]++;
-        counts[d2-1]++;
-        counts[d3-1]++;
-        counts[d4-1]++;
-        counts[d5-1]++;
+        int[] dices = new int[] {d1, d2, d3, d4, d5};
+        int[] counts = getValueOccurrences(dices);
+
         int n = 0;
         int score = 0;
         for (int i = 0; i < 6; i += 1)
@@ -101,99 +90,74 @@ public class Yatzy
             return 0;
     }
 
-    public static int four_of_a_kind(int _1, int _2, int d3, int d4, int d5)
+    public static int four_of_a_kind(int d1, int d2, int d3, int d4, int d5)
     {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[_1-1]++;
-        tallies[_2-1]++;
-        tallies[d3-1]++;
-        tallies[d4-1]++;
-        tallies[d5-1]++;
+        int[] dices = new int[] {d1, d2, d3, d4, d5};
+        int[] counts = getValueOccurrences(dices);
         for (int i = 0; i < 6; i++)
-            if (tallies[i] >= 4)
+            if (counts[i] >= 4)
                 return (i+1) * 4;
         return 0;
     }
 
     public static int three_of_a_kind(int d1, int d2, int d3, int d4, int d5)
     {
-        int[] t;
-        t = new int[6];
-        t[d1-1]++;
-        t[d2-1]++;
-        t[d3-1]++;
-        t[d4-1]++;
-        t[d5-1]++;
+        int[] dices = new int[] {d1, d2, d3, d4, d5};
+        int[] counts = getValueOccurrences(dices);
+
         for (int i = 0; i < 6; i++)
-            if (t[i] >= 3)
+            if (counts[i] >= 3)
                 return (i+1) * 3;
         return 0;
     }
 
     public static int smallStraight(int d1, int d2, int d3, int d4, int d5)
     {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-        if (tallies[0] == 1 &&
-                tallies[1] == 1 &&
-                tallies[2] == 1 &&
-                tallies[3] == 1 &&
-                tallies[4] == 1)
+        int[] dices = new int[] {d1, d2, d3, d4, d5};
+        int[] counts = getValueOccurrences(dices);
+
+        if (counts[0] == 1 &&
+                counts[1] == 1 &&
+                counts[2] == 1 &&
+                counts[3] == 1 &&
+                counts[4] == 1)
             return 15;
         return 0;
     }
 
     public static int largeStraight(int d1, int d2, int d3, int d4, int d5)
     {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-        if (tallies[1] == 1 &&
-                tallies[2] == 1 &&
-                tallies[3] == 1 &&
-                tallies[4] == 1
-                && tallies[5] == 1)
+        int[] dices = new int[] {d1, d2, d3, d4, d5};
+        int[] counts = getValueOccurrences(dices);
+
+        if (counts[1] == 1 &&
+                counts[2] == 1 &&
+                counts[3] == 1 &&
+                counts[4] == 1
+                && counts[5] == 1)
             return 20;
         return 0;
     }
 
     public static int fullHouse(int d1, int d2, int d3, int d4, int d5)
     {
-        int[] tallies;
+        int[] dices = new int[] {d1, d2, d3, d4, d5};
+        int[] counts = getValueOccurrences(dices);
+
         boolean _2 = false;
         int i;
         int _2_at = 0;
         boolean _3 = false;
         int _3_at = 0;
 
-
-
-
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-
         for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 2) {
+            if (counts[i] == 2) {
                 _2 = true;
                 _2_at = i+1;
             }
 
         for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 3) {
+            if (counts[i] == 3) {
                 _3 = true;
                 _3_at = i+1;
             }
@@ -217,5 +181,14 @@ public class Yatzy
             }
         }
         return sum;
+    }
+
+    private static int[] getValueOccurrences(int[] dices) {
+        int[] counts = new int[6];
+        for (int dice : dices)
+        {
+            counts[dice-1]++;
+        }
+        return counts;
     }
 }
